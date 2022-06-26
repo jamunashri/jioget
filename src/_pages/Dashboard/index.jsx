b570a2038caa939d7f1c0818a61a93e54014224c
LAKSHMIBALAJI001 <96902774+LAKSHMIBALAJI001@users.noreply.github.com>
2022-06-26 09:49:04 +0530

    css commit

--- src/_pages/Dashboard/index.jsx
+++ src/_pages/Dashboard/index.jsx
@@ -1,17 +1,21 @@
-import {Chart, FeaturedInfo, Sidebar, WidgetLg, WidgetSm} from "../../_components/index";
+import Chart from "../../_components/Chart/index";
+import Sidebar from "../../_components/Sidebar/index";
+import WidgetSm from "../../_components/WidgetSm/index";
+import WidgetLg from "../../_components/WidgetLg/index";
+import FeaturedInfo from "../../_components/FeaturedInfo/index";
 import "./index.css";
 import { userData } from "../../dummyData";
 import Topbar from "../../_components/Topbar";
 
-export default function Dashboard() {
+export default function dashboard() {
   return (
     <>
     <Topbar />
     <Sidebar />
+    <FeaturedInfo />
     <div className="dashboard">
-      <FeaturedInfo />
       <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
-      <div className="homeWidgets">
+      <div className="dashboardWidgets">
         <WidgetSm/>
         <WidgetLg/>
       </div>
